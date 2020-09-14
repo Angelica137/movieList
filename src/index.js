3d18b7a87d136334e49559276c185198e11b3ffb
Angelica <Angelica137@users.noreply.github.com>
2020-09-14 23:52:53 +0100

    complete styleing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import List from "./containers/List";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import List from "./containers/List";
 
 const App = () => {
   return (
-    <div>
-      <h1>movie list</h1>
+    <div className="container-fluid">
+      <nav className="navbar sticky-top navbar-light bg-dark">
+        <h1 className="navbar-brand text-light">MovieList</h1>
+      </nav>
+
       <List />
     </div>
   );
