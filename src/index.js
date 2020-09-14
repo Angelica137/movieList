616b3852886c0b7cad844bf646f709fb922e8830
Angelica <Angelica137@users.noreply.github.com>
2020-09-14 22:53:02 +0100

    add title

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import List from "./containers/List";
 
 const App = () => {
-  return <List />;
+  return (
+    <div>
+      <h1>movie list</h1>
+      <List />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
