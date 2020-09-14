b0d7efee6c347036cb53d49e017316039a060221
Angelica <Angelica137@users.noreply.github.com>
2020-09-14 21:53:50 +0100

    refactor List

--- src/containers/List.js
+++ src/containers/List.js
@@ -3,7 +3,12 @@ import Card from "../components/Card/Card";
 
 class List extends Component {
   render() {
-    return <Card />;
+    return (
+      <div>
+        <h1>movie list</h1>
+        <Card />
+      </div>
+    );
   }
 }
 
